405f0432b57ad8c87f6ca305e6d605cbef1780ca
Max Nachlinger <max.nachlinger@gmail.com>
2016-07-15 10:51:57 -0700

    Added useful middleware

--- rest/server.js
+++ rest/server.js
@@ -3,33 +3,37 @@ const app = require('express')()
 const bodyParser = require('body-parser')
 const expressLogger = require('express-bunyan-logger')
 const helmet = require('helmet')
-const config = require('../config')
-const widgetRoutes = require('./widgetRoutes')
-const logger = require('./helpers/logger')
 const compression = require('compression')  // for larger APIs do this via Nginx
 const session = require('express-session')
 const LevelStore = require('level-session-store')(session)
+const config = require('../config')
+const widgetRoutes = require('./widgetRoutes')
+const logger = require('./helpers/logger')
+const env = (process.env.NODE_ENV || 'development').toLowerCase();
 
 app.use(expressLogger({logger}))
 app.use(expressLogger.errorLogger({logger}))
 app.use(bodyParser.json())
 app.use(helmet())
-app.use(compression()) // for larger APIs do this via Nginx
 app.use(session({
   secret: config.sessionSecret,
   resave: false,
   saveUninitialized: true,
-  cookie: { secure: true },
-  store: new LevelStore() // for larger, distributed APIs use redis etc
-}));
+  cookie: { secure: env !== 'development' },
+  store: new LevelStore() // for larger, distributed APIs use redis or something else instead
+}))
+app.use(compression()) // for larger APIs gzip via Nginx
 
 app.use((err, req, res, next) => {
   if (res.headersSent) {
     return next(err)
   }
+  logger.error({err})
   res.status(500).send()
 })
 
 app.use(widgetRoutes)
 
-module.exports.start = (callback) => app.listen(config.api.port, callback)
+module.exports.start = (callback) => {
+  app.listen(config.api.port, callback)
+}
